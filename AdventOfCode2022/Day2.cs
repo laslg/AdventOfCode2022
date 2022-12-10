10a561020e5783f4c8d5d1b23b2d1d362945e771
laslg <llg@grnnbk.com>
2022-12-10 22:45:01 +0100

    Day 2 step 2 done

--- AdventOfCode2022/Day2.cs
+++ AdventOfCode2022/Day2.cs
@@ -6,6 +6,9 @@ internal class Day2
     const char my_rock = 'X';
     const char my_paper = 'Y';
     const char my_scissor = 'Z';
+    const char should_lose = 'X';
+    const char should_draw = 'Y';
+    const char should_win = 'Z';
 
     const int rockScore = 1;
     const int paperScore = 2;
@@ -26,33 +29,77 @@ internal class Day2
             var elf = round[0];
             var me = round[2];
 
-            var isWinner = (me == my_rock && elf == elf_scissor)
-                        || (me == my_paper && elf == elf_rock)
-                        || (me == my_scissor && elf == elf_paper);
+            // var isWinner = (me == my_rock && elf == elf_scissor)
+            //             || (me == my_paper && elf == elf_rock)
+            //             || (me == my_scissor && elf == elf_paper);
 
-            if(isWinner)
+            if(me == should_win)
             {
-                totalPoints += (winScore + GetScore(me));
+                totalPoints += (winScore + GetScore(GetWinCounter(elf)));                
                 continue;
             }
 
-            var isDraw = (me == my_rock && elf == elf_rock)
-                    || (me == my_paper && elf == elf_paper)
-                    || (me == my_scissor && elf == elf_scissor);
+            // var isDraw = (me == my_rock && elf == elf_rock)
+            //         || (me == my_paper && elf == elf_paper)
+            //         || (me == my_scissor && elf == elf_scissor);
 
-            if (isDraw)
+            if (me == should_draw)
             {
-                totalPoints += (drawScore + GetScore(me));
+                totalPoints += drawScore + GetScore(GetDrawCounter(elf));
                 continue;
             }
             
-            totalPoints += GetScore(me);
+            totalPoints += GetScore(GetLossCounter(elf));
                        
         }
 
         return totalPoints;
     }
 
+    private static char GetLossCounter(char elf)
+    {
+        switch (elf)
+        {
+            case elf_rock:
+                return my_scissor;
+            case elf_paper:
+                return my_rock;
+            case elf_scissor:
+                return my_paper;
+            default:
+                return '0';
+        }
+    }
+
+    private static char GetWinCounter(char elf)
+    {
+        switch (elf)
+        {
+            case elf_rock:
+                return my_paper;
+            case elf_paper:
+                return my_scissor;
+            case elf_scissor:
+                return my_rock;
+            default:
+                return '0';
+        }
+    }
+
+    private static char GetDrawCounter(char elf)
+    {
+        switch (elf)
+        {
+            case elf_rock:
+                return my_rock;
+            case elf_paper:
+                return my_paper;
+            case elf_scissor:
+                return my_scissor;
+            default:
+                return '0';
+        }
+    }
     private static int GetScore(char me)
     {
         switch (me)
