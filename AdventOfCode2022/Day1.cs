2b93606d93ecd0c3fee176364f8a56ecebca6cd9
Lasse <llg@grnnbk.com>
2022-12-10 13:24:22 +0100

    Add project files.

---
+++ AdventOfCode2022/Day1.cs
@@ -0,0 +1,24 @@
+﻿internal class Day1
+{
+    internal static int Solve()
+    {
+        var input = File.ReadAllLines("./day1_input.txt");
+
+        var elves = new List<int>();
+        var calories = 0;
+        for (int i = 0; i < input.Length; i++)
+        {
+            if (input[i] != string.Empty)
+            {
+                calories += int.Parse(input[i]);
+            }
+            else
+            {
+                elves.Add(calories);
+                calories = 0;
+            }
+        }
+        var ordered = elves.OrderByDescending(c => c);
+        return ordered.Take(3).Sum();
+    }
+}
---
+++ AdventOfCode2022/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+Console.WriteLine("Hello, World!");
+
+var day1Result = Day1.Solve();
+
+Console.WriteLine($"Day 1: {day1Result}");
+
+
+
+
+
+Console.WriteLine("Press any key ...");
