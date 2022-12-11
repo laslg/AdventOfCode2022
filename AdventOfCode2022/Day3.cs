fbed80b377e72736c240ff34b883519c37a1bd67
laslg <llg@grnnbk.com>
2022-12-11 11:33:15 +0100

    Day 3 step 2 done

--- AdventOfCode2022/Day3.cs
+++ AdventOfCode2022/Day3.cs
@@ -4,16 +4,29 @@ internal class Day3
     {
         var input = File.ReadAllLines("./day3_input.txt");
 
-        var prioritySum = 0;
+        var teams = new List<string[]>();
+        var team = new List<string>();
         for (int i = 0; i < input.Length; i++)
         {
-            var sack = input[i];
-            var half = sack.Length/2;
-            var comp1 = sack.Substring(0, half);
-            var comp2 = sack.Substring(half);
+            team.Add(input[i]);            
 
-            var sharedChar = comp1.First(c => comp2.IndexOf(c) > -1);
-            var charValue = vals.IndexOf(sharedChar) + 1;
+            if (team.Count == 3)
+            {
+                teams.Add(team.ToArray());
+                team.Clear();
+            }            
+        }
+
+        var prioritySum = 0;
+        for (int i = 0; i < teams.Count; i++)
+        {
+            var badge = teams[i][0].Intersect(teams[i][1]).Intersect(teams[i][2]).Single();
+            // var sack = input[i];
+            // var half = sack.Length/2;
+            // var comp1 = sack.Substring(0, half);
+            // var comp2 = sack.Substring(half);
+            //var sharedChar = comp1.First(c => comp2.IndexOf(c) > -1);
+            var charValue = vals.IndexOf(badge) + 1;
             prioritySum += charValue;
         }
 
