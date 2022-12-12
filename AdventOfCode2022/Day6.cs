ca8159212ec7a068a5df44d13fe85ab1daa8a694
laslg <llg@grnnbk.com>
2022-12-12 21:34:42 +0100

    Day 6 step 2 done

--- AdventOfCode2022/Day6.cs
+++ AdventOfCode2022/Day6.cs
@@ -5,25 +5,41 @@
         var input = File.ReadAllText("./day6_input.txt");
 
         int index = 0;
-        var chars = new char[4];
+        var chars = new char[14];
         for (index = 0; index < input.Length; index++)
         {
-            chars[0] = input[index];
-            chars[1] = input[index + 1];
-            chars[2] = input[index + 2];
-            chars[3] = input[index + 3];
-            if (chars[0] != chars[1]
-                && chars[0] != chars[2]
-                && chars[0] != chars[3]
-                && chars[1] != chars[2]
-                && chars[1] != chars[3]
-                && chars[2] != chars[3])
+            for (int i = 0; i < 14; i++)
+            {
+                chars[i] = input[index + i];
+            }
+
+            if (CheckArray(chars))
             {
                 break;
             }
+
+            //chars[0] = input[index];
+            //chars[1] = input[index + 1];
+            //chars[2] = input[index + 2];
+            //chars[3] = input[index + 3];
+            //if (chars[0] != chars[1]
+            //    && chars[0] != chars[2]
+            //    && chars[0] != chars[3]
+            //    && chars[1] != chars[2]
+            //    && chars[1] != chars[3]
+            //    && chars[2] != chars[3])
+            //{
+            //    break;
+            //}
         }
 
-        int numberOfChars = index + 4;
+        int numberOfChars = index + 14;
         return numberOfChars;
+    }
+
+    private static bool CheckArray(char[] chars)
+    {
+        var hs = new HashSet<char>(chars);
+        return hs.Count == chars.Length;
     }
 }
